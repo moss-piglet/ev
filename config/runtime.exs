1be7f0215ea3a75f758dbad093f661f71acfda7b
f0rest8 <markthayer@hey.com>
2023-07-21 19:48:34 -0400

    Output host

--- config/runtime.exs
+++ config/runtime.exs
@@ -50,6 +50,7 @@ if config_env() == :prod do
 
   host = System.get_env("PHX_HOST") || "metamorphic.app"
   port = String.to_integer(System.get_env("PORT") || "4000")
+  IO.puts "HOST: #{host}"
 
   config :metamorphic, MetamorphicWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
