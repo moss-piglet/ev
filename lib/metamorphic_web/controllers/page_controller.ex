3c136a7029488d46bc062e5ac5d06d9a5a4fbcc5
mark <mark@macbook-pro.mynetworksettings.com>
2023-07-30 22:42:16 -0400

    Add page title welcome

--- lib/metamorphic_web/controllers/page_controller.ex
+++ lib/metamorphic_web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule MetamorphicWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, page_title: "Welcome", layout: false)
   end
 end
