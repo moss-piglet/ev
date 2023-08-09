541fd2311dd737efbda4eb5fa89ae0e7a2d6eba9
Mark Thayer <markthayer@hey.com>
2023-08-09 12:15:44 -0400

    Add favicon

--- lib/metamorphic_web.ex
+++ lib/metamorphic_web.ex
@@ -17,7 +17,7 @@ defmodule MetamorphicWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets favicon fonts images favicon.ico robots.txt)
 
   def router do
     quote do
