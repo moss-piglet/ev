4b996f520c1c2d08ca0a759ff3f904072bd495fd
Mark Thayer <markthayer@hey.com>
2023-08-20 15:05:00 -0400

    Update release for migrations

--- lib/metamorphic/release.ex
+++ lib/metamorphic/release.ex
@@ -25,4 +25,41 @@ defmodule Metamorphic.Release do
   defp load_app do
     Application.load(@app)
   end
+
+  @doc """
+  Print the migration status for configured Repos' migrations.
+  """
+  def migration_status do
+    for repo <- repos(), do: print_migrations_for(repo)
+  end
+
+  defp print_migrations_for(repo) do
+    paths = repo_migrations_path(repo)
+
+    {:ok, repo_status, _} =
+      Ecto.Migrator.with_repo(repo, &Ecto.Migrator.migrations(&1, paths), mode: :temporary)
+
+    IO.puts(
+      """
+      Repo: #{inspect(repo)}
+       Status  Migration ID  Migration Name
+      --------------------------------------------------
+      """ <>
+        Enum.map_join(repo_status, "\n", fn {status, number, description} ->
+          " #{pad(status, 10)}#{pad(number, 16)}#{description}"
+        end) <> "\n"
+    )
+  end
+
+  defp repo_migrations_path(repo) do
+    config = repo.config()
+    priv = config[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
+    config |> Keyword.fetch!(:otp_app) |> Application.app_dir() |> Path.join(priv)
+  end
+
+  defp pad(content, pad) do
+    content
+    |> to_string
+    |> String.pad_trailing(pad)
+  end
 end
